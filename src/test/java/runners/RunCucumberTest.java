3c4c157466baf6e982ca85039ebc3ab40ba808f7
m481499 <gabriel.hd.oliveira@bradesco.com.br>
2025-10-31 16:01:45 -0300

    Alteração no ci.yalm

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -1,16 +1,16 @@
 package runners;
 
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"steps", "hooks"},
-        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
-        monochrome = true
+        glue = {"steps"},
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+        monochrome = true,
+        publish = true
 )
-
 public class RunCucumberTest {
 }
